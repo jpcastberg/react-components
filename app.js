4c9f78ab711b78447801f0bf314c42e6f6409b1b
jpcastberg <jpcastberg@gmail.com>
2019-01-16 18:56:07 -0800

    Complete react-components course

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 // TODO
+var GroceryList = (props) => (
+  <ul>
+    <GroceryListItem item={props.items[0]}/>
+    <GroceryListItem item={props.items[1]}/>
+    <GroceryListItem item={props.items[2]}/>
+    <GroceryListItem item={props.items[3]}/>
+  </ul>
+);
+
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props);  
+
+    this.state = {
+      hover: false
+    };
+  }
+  onListItemHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+  render() {
+    
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+
+ReactDOM.render(<GroceryList items={['apple', 'pineApple', 'bananas','orange']}/>, document.getElementById('app')); 
+
